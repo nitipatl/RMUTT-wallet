96a7250aa0c100dc7e14b56ad0980ee7eb9df0b4
Pongpon Nilaphruek New MAC <pongpon@Pongpons-MacBook-Pro.local>
2018-03-29 16:19:31 +0700

    test account API

--- API/account/Account.php
+++ API/account/Account.php
@@ -12,5 +12,13 @@
  * @author pongpon
  */
 class Account {
-    //put your code here
+
+    public function getAccount($userID) {
+        $accounts[] = array("accountID" => "1101041", "accountName" => "ค่าใช้จ่ายทั่วไป", "balance" => 44000);
+        $accounts[] = array("accountID" => "1101042", "accountName" => "เงินสะสม", "balance" => 2000);
+        $data["accounts"] = $accounts;
+        $jsonData = json_encode($data);
+        return $jsonData;
+    }
+
 }
---
+++ API/account/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once './Account.php';
+$account = new Account();
+print $account->getAccount($_GET['userID']);
+?>

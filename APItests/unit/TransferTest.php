0aceeae2ccd528bec049d9693e1fa121a0395bfd
Nitipat L <iamsvz@gmail.com>
2018-03-30 15:02:30 +0700

    refactor test

--- APItests/unit/TransferTest.php
+++ APItests/unit/TransferTest.php
@@ -3,19 +3,20 @@ include dirname(__FILE__).'/../../API/transfer/Transfer.php';
 use PHPUnit\Framework\TestCase;
 
 class TransferTest extends TestCase{
+    protected $transfer;
+    function setUp() {
+        $this->transfer = new Transfer();
+    }
     function testInput20000ReturnFalse() {
-        $transfer = new Transfer();
-        $action = $transfer->isOverLimitBOT(20000);
+        $action = $this->transfer->isOverLimitBOT(20000);
         $this->assertEquals(false, $action);
     }
     function testInput50001ReturnTrue() {
-        $transfer = new Transfer();
-        $action = $transfer->isOverLimitBOT(50001);
+        $action = $this->transfer->isOverLimitBOT(50001);
         $this->assertEquals(true, $action);
     }
     function testInput50000ReturnFalse() {
-        $transfer = new Transfer();
-        $action = $transfer->isOverLimitBOT(50000);
+        $action = $this->transfer->isOverLimitBOT(50000);
         $this->assertEquals(false, $action);
     }
 }
